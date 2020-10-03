f26a89890cbb6bbbd80ddc4fa0370736aef016b7
Jeff Casimir <jeff@casimircreative.com>
2020-10-03 17:53:47 -0600

    Extracts total expected listings

--- lib/listing.rb
+++ lib/listing.rb
@@ -1,3 +1,34 @@
 class Listing
 	
 end
+
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+class ListingIndex
+  
+  attr_reader :source_document, :total_listings
+  
+  def initialize  
+  end
+  
+  def default_url
+    "https://grand.coloradotaxsale.com/index.cfm?folder=previewitems"
+  end
+  
+  def fetch
+    @source_document = Nokogiri::HTML(URI.open(default_url))
+    parse
+  end
+
+  def parse
+    parse_listings_count
+  end
+  
+  def parse_listings_count
+    container = source_document.at('div#paginationToolbar')
+    @total_listings = container.text().match(/of (\d+) records/)[1].to_i
+  end
+  
+end
--- test/listing_test.rb
+++ test/listing_test.rb
@@ -6,4 +6,21 @@ class ListingTest < Minitest::Test
 	def test_it_exists
     assert Listing.new
   end
+  
+end
+
+class ListingIndexTest < Minitest::Test
+  def test_it_exists
+    assert ListingIndex.new  
+  end
+  
+  def test_it_fetches
+    assert ListingIndex.new.fetch
+  end
+  
+  def test_it_has_a_total_record_count
+    index = ListingIndex.new
+    index.fetch
+    assert index.total_listings > 100
+  end
 end
